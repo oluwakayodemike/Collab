a56836b6a8668fe082e4fa267fd9016d18a98ec2
Oluwakayode Michael <104535663+oluwakayodemike@users.noreply.github.com>
2023-09-27 00:10:14 +0000

    initial files

---
+++ BACKEND/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const http = require("http");
+const Ably = require('ably');
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Realtime with Ably!");
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log("Server is running on localhost:" + port)
+});
