9e9cb98a4a8da27c559c23a433d4cccfe93852ef
Oluwakayode Michael <104535663+oluwakayodemike@users.noreply.github.com>
2023-10-05 14:50:36 +0000

    feat: added a close button to the popup menu, update still required tho... - issue: after the popup has been closed, popup is not opening again

--- FRONTEND/client/src/components/Popup/Popup.jsx
+++ FRONTEND/client/src/components/Popup/Popup.jsx
@@ -1,26 +1,44 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./Popup.css";
 import { FaClipboard } from "react-icons/fa";
 
 const Popup = () => {
   const [copySuccess, setCopySuccess] = useState(false);
+  const [isPopupOpen, setIsPopupOpen] = useState(true);
+
+  // if isPopupOpen == true;
+  // setIs p
+    const handleClosePopup = () => {
+      setIsPopupOpen(false);
+    };
+
   return (
-    <div className="popup-container">
-      <div className="popup">
-        <h3 className="heading">Join the Session</h3>
-        <p className="description">
-          This is the room code for the Session. Share it with anyone you want
-          to invite.
-        </p>
-        <div className="code-container">
-          <h3 className="code">123456</h3>
-          <button className="copy-button" onClick={()=>setCopySuccess(true)}>
-            <FaClipboard />
-            {copySuccess?'Copied':'Copy'}
-          </button>
+    <>
+      {isPopupOpen && (
+        <div className="popup-container">
+          <div className="popup">
+            <h3 className="heading">Join the Session</h3>
+            <p className="description">
+              This is the room code for the Session. Share it with anyone you
+              want to invite.
+            </p>
+            <div className="code-container">
+              <h3 className="code">123456</h3>
+              <button
+                className="copy-button"
+                onClick={() => setCopySuccess(true)}
+              >
+                <FaClipboard />
+                {copySuccess ? "Copied" : "Copy"}
+              </button>
+            </div>
+            <button className="close-button" onClick={handleClosePopup}>
+              Close
+            </button>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
