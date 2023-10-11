0313712b010a7eda4055e112b0695f41d595019d
Oluwakayode Michael <104535663+oluwakayodemike@users.noreply.github.com>
2023-10-11 22:30:14 +0000

    feat: created the whiteboarding area. - set up the logic for the pencil, line & rectangle tools. - added pencil image as toolbar.

--- FRONTEND/client/src/components/Room/Room.jsx
+++ FRONTEND/client/src/components/Room/Room.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from 'react';
 import "./Room.css"
 import Board from "./WhiteBoard/Board"
-
+import PencilIcon from "../../assets/ToolBar/meeee.png"
 const Whiteboard = () => {
     const canvas = useRef(null);
     const ctx = useRef(null);
@@ -9,38 +9,27 @@ const Whiteboard = () => {
     const [color, setColor] = useState("black");
     const [elements, setElements] = useState([]);
 
+    const handleToolClick = (selectedTool) => {
+        setTool(selectedTool);
+    };
+
     return (
         <div className='row'>
             <div>
                 < Board canvasRef={canvas} ctxRef={ctx} elements={elements} setElements={setElements} color={color} tool={tool}  />
             </div>
             
+            {/* set for "rect",  "pencil", "line", and circle */}
             <div className="toolbox">
-                <img src={PencilIcon} alt="Pencil Tool" onClick={() => setTool("pencil")} />
-                <img src={LineIcon} alt="Line Tool" onClick={() => setTool("line")} />
+                <img
+                    src={PencilIcon}
+                    alt="Pencil Tool"
+                    onClick={() => handleToolClick("pencil")}
+                    className={tool === "pencil" ? "selected" : ""}
+                />
             </div>
-            {/* <h1 className='text-center py-8'>Whiteboarding App</h1> */}
-            {/* <div className='col-md-12 mt-4 mb-5 d-flex align-items-center justify-content-around'>
-                <div className='d-flex col-md-4 justify-content-between gap-1'>
-                    <div className='d-flex gap-1'>
-                        <label for="pencil">Pencil</label>
-                        <input type="radio" name='tool' id="pencil" value='pencil' onChange={(e)=>setTool(e.target.value)}/>
-                    </div>
-                    <div className='d-flex gap-1'>
-                        <label for="line">Line</label>
-                        <input type="radio" name='tool' id="line" value='line' onChange={(e)=>setTool(e.target.value)}/>
-                    </div>
-                    <div className='d-flex gap-1'>
-                        <label for="circle">Circle</label>
-                        <input type="radio" name='tool' id="circle" value='circle' onChange={(e)=>setTool(e.target.value)}/>
-                    </div>
-                    <div className='d-flex gap-1'>
-                        <label for="rect">Rectangle</label>
-                        <input type="radio" name='tool' id="rect" value='rect' onChange={(e)=>setTool(e.target.value)}/>
-                    </div>
-                </div>
-            </div> */}
 
+            {/* color feature soon... */}
             {/* <div className='col-md-3 mx-auto'>
                 <div className='d-flex justify-content-center'>
                     <label htmlFor="color">Select Color:</label>
