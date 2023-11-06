f821c929e45085033e92358d826177b8a77a629a
Oluwakayode Michael <104535663+oluwakayodemike@users.noreply.github.com>
2023-11-06 19:53:54 +0000

    feat, updates, refactor: updated the code to save and load App state locally - App State is getting saved by id locally. - started working on the NavBar for the Room. - would migrate to store App State in Database later

--- FRONTEND/client/src/components/Room/Header/Header.jsx
+++ FRONTEND/client/src/components/Room/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import End from "../../../assets/ToolBar/end-call.svg";
 import "./Header.css";
 
 const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light custom-navbar">
-      <div className="container">
+      <div className="end">
+        <img 
+          src={End}
+          alt="End Button" 
+        />
       </div>
     </nav>
   );
