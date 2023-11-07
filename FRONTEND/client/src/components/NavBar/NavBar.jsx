4090a42486a11ed650bd356b28c42f977d12384f
Oluwakayode Michael <104535663+oluwakayodemike@users.noreply.github.com>
2023-11-07 23:54:53 +0000

    feat: added fallback to `/` from signin in navBar.

--- FRONTEND/client/src/components/NavBar/NavBar.jsx
+++ FRONTEND/client/src/components/NavBar/NavBar.jsx
@@ -3,24 +3,21 @@ import './NavBar.css';
 import { useNavigate } from 'react-router-dom';
 import { useUser, UserButton } from "@clerk/clerk-react";
 
-
 const NavBar = () => {
     const user = useUser();
     const [currentDateTime, setCurrentDateTime] = useState(new Date());
     const [isDarkMode, setIsDarkMode] = useState(false);
-    // const { isDarkMode, toggleDarkMode } = useContext(ThemeContext);
 
     useEffect(() => {
         const intervalId = setInterval(() => {
-        setCurrentDateTime(new Date());
+            setCurrentDateTime(new Date());
         }, 1000);
 
         return () => {
-        clearInterval(intervalId);
+            clearInterval(intervalId);
         };
     }, []);
 
-    // time and date format
     const options = {
         hour: 'numeric',
         minute: 'numeric',
@@ -32,40 +29,51 @@ const NavBar = () => {
 
     const formattedDate = new Intl.DateTimeFormat('en-US', options).format(currentDateTime);
 
-    // toggle light and dark mode
     const toggleDarkMode = () => {
         setIsDarkMode(!isDarkMode);
     };
 
-    // modes changes...
     const dateClasses = `mr-2 ${isDarkMode ? 'text-light' : ''}`;
     const iconColor = isDarkMode ? 'white' : 'black';
 
     const navigate = useNavigate();
 
+    const handleSignInClick = () => {
+        localStorage.setItem("comebackUrl", window.location.pathname);
+        navigate('/sign-in');
+    };
+
+    useEffect(() => {
+        const comebackUrl = localStorage.getItem("comebackUrl");
+        if (comebackUrl) {
+            localStorage.removeItem("comebackUrl");
+            navigate(comebackUrl);
+        }
+    }, []);
+
     return (
         <nav className={`navbar navbar-expand-lg navbar-custom ${isDarkMode ? 'navbar-dark bg-dark' : 'navbar-light bg-light'}`}>
             <div className="container">
-            <a className="navbar-brand" href="#">
-                <img src="" alt="Logo" width="30" height="30" className="d-inline-block align-top" />
-                Collab
-            </a>
-            <div className="ml-auto">
-                <i
-                className={`fa fa-${isDarkMode ? 'sun-o' : 'moon-o'} fa-icon`}
-                style={{ color: iconColor }}
-                onClick={toggleDarkMode}
-                />
-                <span className={`date ${dateClasses}`}>{formattedDate}</span>
-                {user.isSignedIn ? (
-                    <UserButton className="user-button" />
-                ) : (
-                    <button className="btn btn-primary btn-signin" onClick={() => navigate('/sign-in')}>Sign In</button>
-                )}
-            </div>
+                <a className="navbar-brand" href="#">
+                    <img src="" alt="Logo" width="30" height="30" className="d-inline-block align-top" />
+                    Collab
+                </a>
+                <div className="ml-auto">
+                    <i
+                    className={`fa fa-${isDarkMode ? 'sun-o' : 'moon-o'} fa-icon`}
+                    style={{ color: iconColor }}
+                    onClick={toggleDarkMode}
+                    />
+                    <span className={`date ${dateClasses}`}>{formattedDate}</span>
+                    {user.isSignedIn ? (
+                        <UserButton className="user-button" />
+                    ) : (
+                        <button className="btn btn-primary btn-signin" onClick={handleSignInClick}>Sign In</button>
+                    )}
+                </div>
             </div>
         </nav>
     );
 };
 
-export default NavBar
+export default NavBar;
