cf3ecb80ff9b5f08558075b2554ff261d32e36cf
Oluwakayode Michael <104535663+oluwakayodemike@users.noreply.github.com>
2023-11-07 23:49:16 +0000

    fix: !SignedIn user cannot create a meeting now! - user that are not signed in cannot get a meeting link now. - so had to result in using another localStorage to storing things again (another dumb iteration i know, haha) - prevLink get stored and and used as fallback url.

--- FRONTEND/client/src/components/Room/Header/Header.jsx
+++ FRONTEND/client/src/components/Room/Header/Header.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import End from "../../../assets/ToolBar/end-call.svg";
 import "./Header.css";
 
 const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light custom-navbar">
-      <div className="end">
-        <img 
-          src={End}
-          alt="End Button" 
-        />
+      <div className="leave justify-content-end">
+        <button
+          className="btn btn-outline-danger"
+        >
+          Leave
+        </button>
       </div>
     </nav>
   );
